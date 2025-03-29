58463dab1302efa5797e55bbe3397637ebb2209f
Alonso Sánchez López <alosan02@ucm.es>
2025-03-29 22:30:46 +0100

    Update inputoutput.h

--- inputoutput.h
+++ inputoutput.h
@@ -4,7 +4,25 @@
 #include <string>
 #include <iomanip>
 #include "juego.h"
+#include "colors.h"
 
+const int N_HUECOS = 2;
+const int MAX_FILS = 5;
+const int MAX_COLS = 5;
+
+typedef enum { NUMERO, VACIA, MINA } tEstado;
+
+typedef struct {
+    bool visible; /* celda descubierta */
+    tEstado estado; /* tipo de celda */
+    int numero; /* numero de bombas que la rodean */
+    bool marcada; /* para marcar posiciones de las minas */
+} tElemento;
+
+typedef tElemento tMatriz[MAX_FILS][MAX_COLS];ç
+
+
+void mostrar_matriz(const tMatriz& matriz);
 void mostrar_cabecera();
 void pedir_pos(int& fila, int& columna);
 void mostrar_resultado(const tJuego& juego);
